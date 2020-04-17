921bea1485880bc7a4ba6218dc12bf1478c3de47
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-17 21:01:52 +0900

    Initial commit

---
+++ 01_ES6_言語仕様/06_定数.js
@@ -0,0 +1,13 @@
+/**
+ * * 定数は let の代わりに const を用いて定義します。
+ * 定数は変数と違って一度代入された値を更新することができません。
+ */
+
+// 定数languageを定義してください
+const language = 'フランス語';
+
+// 定数languageの値を出力してください
+console.log(language);
+
+// 定数languageを用いて、「〇〇を話せます」と出力してください
+console.log(language + 'を話せます');
